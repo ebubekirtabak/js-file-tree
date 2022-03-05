a1dc111f3e0d9b21388283296aa6356c5ffbaa55
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-06 00:00:29 +0300

    add explorer folder view in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.scss';
+import ExplorerFolderView from './components/ExplorerFolderView/ExplorerFolderView';
+
+const dir = [
+  { 
+    type: 'folder',
+    name: 'Home',
+    path: '/Home',
+    files: [
+      { type: 'file', name: 'note.txt' },
+      { type: 'folder', name: 'User', path: '/Home/User',
+        files: [
+          { type: 'file', path: '/Home/User/UserSettings.txt', name: 'UserSettings.txt' },
+          { type: 'file', path: '/Home/User/UserNotes.txt', name: 'UserNotes.txt' },
+          { type: 'file', path: '/Home/User/UserSettings(2).txt', name: 'UserSettings(2).txt' },
+        ]
+      }
+    ]
+  },
+  { type: 'file', name: '.zshrc' },
+];
 
 function App() {
   return (
     <div className="App">
-
+      <ExplorerFolderView dir={dir} />
     </div>
   );
 }
