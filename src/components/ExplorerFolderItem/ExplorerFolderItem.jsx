5ec699c204ded95d85546ac7d2dd14f2ef3b0e93
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-06 00:00:42 +0300

    fix: key warnings

--- src/components/ExplorerFolderItem/ExplorerFolderItem.jsx
+++ src/components/ExplorerFolderItem/ExplorerFolderItem.jsx
@@ -3,7 +3,7 @@ import ExplorerFileItem from "../ExplorerFileItem/ExplorerFileItem";
 
 const ExplorerFolderItem = (props) => {
     const { folder } = props;
-    const { name, files = [] } = folder;
+    const { name, files = [], path } = folder;
     const [ active, setActive ] = useState(false);
     const folders = files.filter(({ type }) => type === "folder");
     const fileList = files.filter(({ type }) => type === "file");
@@ -11,6 +11,7 @@ const ExplorerFolderItem = (props) => {
         <div className="explorer-folder-item">
             <div 
                 className={`explorer-folder-item__head ${active ? "active" : ""}`}
+                key={path}
                 onClick={() => setActive(!active)}
                 >
                 <span className="material-icons">folder</span>
@@ -18,8 +19,8 @@ const ExplorerFolderItem = (props) => {
                 <span className="material-icons explorer-folder-item__indicator">arrow_right</span>
             </div>
             <div className={`explorer-folder-item__list ${active ? "active" : ""}`}>
-                { folders.map((folder) => <ExplorerFolderItem folder={folder} /> ) }
-                { fileList.map((file) => <ExplorerFileItem file={file} />) }
+                { folders.map((folder) => <ExplorerFolderItem folder={folder} key={folder.path} /> ) }
+                { fileList.map((file) => <ExplorerFileItem file={file} key={file.path} />) }
             </div>
         </div>
     );
