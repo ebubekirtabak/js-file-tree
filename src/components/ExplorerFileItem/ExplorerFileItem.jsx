26c132fcf16193a98eb8cd64902fbb1bb586baff
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-06 22:30:58 +0300

    add editor and context menu in file item

--- src/components/ExplorerFileItem/ExplorerFileItem.jsx
+++ src/components/ExplorerFileItem/ExplorerFileItem.jsx
@@ -1,12 +1,35 @@
+import ExplorerContextMenu from "../ExplorerContextMenu/ExplorerContextMenu";
+import { useState } from 'react';
+import ItemEditor from '../ItemEditor/ItemEditor';
+
 const ExplorerFileItem = (props) => {
     const { file } = props;
     const { name } = file;
+    const [showContextMenu, setShowContextMenu] = useState(false);
+    const [contextAction, setContextAction] = useState('');
     return (    
         <div className="explorer-file-item">
-            <div className="explorer-file-item__head">
+            {
+                contextAction === 'edit' && (<ItemEditor value={name} onSubmitNewName={(newName) => {
+                    console.log(newName);
+                    setContextAction('');
+                }} />)
+            }
+            <div 
+                className={`explorer-file-item__head ${contextAction === 'edit' ? 'hide' : ''}`}
+                onContextMenu={(event) => {
+                    event.preventDefault();
+                    setShowContextMenu(!showContextMenu);
+                }}
+                data-testid="file-item-head"
+            >
                 <span className="material-icons">description</span>
                 { name }
             </div>
+            <ExplorerContextMenu show={showContextMenu} handleMenuItemClick={(type) => {
+                setShowContextMenu(false)
+                setContextAction(type)
+            }} />
         </div>
     );
 };
--- src/components/ExplorerFileItem/ExplorerFileItem.test.js
+++ src/components/ExplorerFileItem/ExplorerFileItem.test.js
@@ -1,5 +1,6 @@
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
 import ExplorerFileItem from './ExplorerFileItem';
 
 const file = {
@@ -20,7 +21,39 @@ describe('ExplorerFileItem Component', () => {
   it('renders file name', () => {
     const div = document.createElement('div');
     render(<ExplorerFileItem file={file} />, div);
-    const linkElement = screen.getByText(/explorer.js/i);
-    expect(linkElement).toBeInTheDocument();
+    const fileElement = screen.getByText(/explorer.js/i);
+    expect(fileElement).toBeInTheDocument();
   });
+
+  it('renders context menu when onContext', async () => {
+        const div = document.createElement('div');
+        render(<ExplorerFileItem file={file} />, div);
+        const fileElement = screen.getByTestId('file-item-head');
+        const contextMenu = screen.getByTestId('context-menu');
+        fireEvent.contextMenu(fileElement, {
+            preventDefault: () => {}
+        });
+        await new Promise((r) => setTimeout(r, 2000));
+        expect(contextMenu).toBeVisible();
+
+        const contextItem = screen.getAllByTestId('context-menu-item');
+        expect(contextItem[0]).toBeInTheDocument();
+        fireEvent.click(contextItem[0]);
+
+        const itemEditor = screen.getByTestId('item-editor');
+        expect(itemEditor).toBeVisible();
+
+        const inputElement = screen.getByRole('textbox', { Name: 'newName' });
+        expect(inputElement).toBeInTheDocument();
+        fireEvent.change(inputElement, { target: { value: 'test(2).txt' } })
+        fireEvent.keyUp(inputElement, {
+            key: "Enter",
+            code: "Enter",
+            keyCode: 13,
+            charCode: 13,
+            which: 13,
+        });
+        expect(itemEditor).not.toBeVisible();
+  });
+
 });
