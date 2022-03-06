48d86416ffee6d79764ab51326d40a0aeb64492d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-07 00:05:27 +0300

    add directory reducers in root reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
+import { directoryReducers } from './DirectoryReducers';
 
 const rootReducer = combineReducers({
-
-
+    directoryReducers
 });
  
 export default rootReducer;
