a0f5827446b0c5df899def562bc17ca4b1e6d139
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-07 00:06:34 +0300

    use fetchDirData action to load dir

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import './App.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchDirData } from './store/actions/DirectoryActions';
 import ExplorerFolderView from './components/ExplorerFolderView/ExplorerFolderView';
+import './App.scss';
 
-const dir = [
-  { 
-    type: 'folder',
-    name: 'Home',
-    path: '/Home',
-    files: [
-      { type: 'file', name: 'note.txt' },
-      { type: 'folder', name: 'User', path: '/Home/User',
-        files: [
-          { type: 'file', path: '/Home/User/UserSettings.txt', name: 'UserSettings.txt' },
-          { type: 'file', path: '/Home/User/UserNotes.txt', name: 'UserNotes.txt' },
-          { type: 'file', path: '/Home/User/UserSettings(2).txt', name: 'UserSettings(2).txt' },
-        ]
-      }
-    ]
-  },
-  { type: 'file', name: '.zshrc' },
-];
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDirData());
+  });
+
   return (
     <div className="App">
-      <ExplorerFolderView dir={dir} />
+      <ExplorerFolderView  />
     </div>
   );
 }
