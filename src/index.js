a5ba627f7e7a55543b0358b5a2e7a7b3bdd2068b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-06 23:37:37 +0300

    install redux package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import store from './store/store'
+import { Provider } from 'react-redux'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
