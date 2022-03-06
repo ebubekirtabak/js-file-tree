2e2950b72351a79f2d717a041a168f67a98b4bbe
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-06 22:53:33 +0300

    add editor and context menu in folder item

--- src/components/ExplorerFolderItem/ExplorerFolderItem.jsx
+++ src/components/ExplorerFolderItem/ExplorerFolderItem.jsx
@@ -1,24 +1,47 @@
 import { useState } from "react";
 import ExplorerFileItem from "../ExplorerFileItem/ExplorerFileItem";
+import ItemEditor from '../ItemEditor/ItemEditor';
+import ExplorerContextMenu from '../ExplorerContextMenu/ExplorerContextMenu';
 
 const ExplorerFolderItem = (props) => {
     const { folder } = props;
     const { name, files = [], path } = folder;
     const [ active, setActive ] = useState(false);
+    const [showContextMenu, setShowContextMenu] = useState(false);
+    const [contextAction, setContextAction] = useState('');
+
     const folders = files.filter(({ type }) => type === "folder");
     const fileList = files.filter(({ type }) => type === "file");
     return (
         <div className="explorer-folder-item">
+            {
+                contextAction === 'edit' && (<ItemEditor value={name} onSubmitNewName={(newName) => {
+                    console.log(newName);
+                    setContextAction('');
+                }} />)
+            }
             <div 
-                className={`explorer-folder-item__head ${active ? "active" : ""}`}
+                className={`explorer-folder-item__head ${active ? "active" : ""} ${contextAction === 'edit' ? 'hide' : ''}`}
                 key={path}
                 onClick={() => setActive(!active)}
+                onContextMenu={(event) => {
+                    event.preventDefault();
+                    setShowContextMenu(!showContextMenu);
+                }}
+                data-testid="folder-item-head"
                 >
                 <span className="material-icons">folder</span>
                 { name }
                 <span className="material-icons explorer-folder-item__indicator">arrow_right</span>
             </div>
-            <div className={`explorer-folder-item__list ${active ? "active" : ""}`}>
+            <ExplorerContextMenu show={showContextMenu} handleMenuItemClick={(type) => {
+                setShowContextMenu(false)
+                setContextAction(type)
+            }} />
+            <div 
+                className={`explorer-folder-item__list ${active ? "active" : ""}`}
+                data-testid="folder-item-list"
+            >
                 { folders.map((folder) => <ExplorerFolderItem folder={folder} key={folder.path} /> ) }
                 { fileList.map((file) => <ExplorerFileItem file={file} key={file.path} />) }
             </div>
--- src/components/ExplorerFolderItem/ExplorerFolderItem.test.js
+++ src/components/ExplorerFolderItem/ExplorerFolderItem.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ExplorerFolderItem from './ExplorerFolderItem';
 const folder = {
     type: 'folder',
@@ -10,5 +10,48 @@ describe('ExplorerFolderItem Component', () => {
       render(<ExplorerFolderItem folder={folder} />);
       const folderElement = screen.getByText(/Home/i);
       expect(folderElement).toBeInTheDocument();
+    });
+
+    it('should active when the user clicks', async () => {
+        const div = document.createElement('div');
+        render(<ExplorerFolderItem folder={folder} />, div);
+        const folderElement = screen.getByTestId('folder-item-head');
+        const folderItemList = screen.getByTestId('folder-item-list');
+        // expect(folderItemList).toHaveStyle({ visibility: 'hidden'});
+        fireEvent.click(folderElement, {
+            preventDefault: () => {}
+        });
+        expect(folderItemList).toBeVisible();
+    });
+
+    it('renders context menu when onContext', async () => {
+        const div = document.createElement('div');
+        render(<ExplorerFolderItem folder={folder} />, div);
+        const folderElement = screen.getByTestId('folder-item-head');
+        const contextMenu = screen.getByTestId('context-menu');
+        fireEvent.contextMenu(folderElement, {
+            preventDefault: () => {}
+        });
+        await new Promise((r) => setTimeout(r, 2000));
+        expect(contextMenu).toBeVisible();
+
+        const contextItem = screen.getAllByTestId('context-menu-item');
+        expect(contextItem[0]).toBeInTheDocument();
+        fireEvent.click(contextItem[0]);
+
+        const itemEditor = screen.getByTestId('item-editor');
+        expect(itemEditor).toBeVisible();
+
+        const inputElement = screen.getByRole('textbox', { Name: 'newName' });
+        expect(inputElement).toBeInTheDocument();
+        fireEvent.change(inputElement, { target: { value: 'test(2).txt' } })
+        fireEvent.keyUp(inputElement, {
+            key: "Enter",
+            code: "Enter",
+            keyCode: 13,
+            charCode: 13,
+            which: 13,
+        });
+        expect(itemEditor).not.toBeVisible();
     });
 });
