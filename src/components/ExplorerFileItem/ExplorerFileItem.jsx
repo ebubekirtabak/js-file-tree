bec71331f096ae912057394e8994c0b6f653e731
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-03-07 10:13:19 +0300

    fix: indent

--- src/components/ExplorerFileItem/ExplorerFileItem.jsx
+++ src/components/ExplorerFileItem/ExplorerFileItem.jsx
@@ -3,35 +3,36 @@ import { useState } from 'react';
 import ItemEditor from '../ItemEditor/ItemEditor';
 
 const ExplorerFileItem = (props) => {
-    const { file } = props;
-    const { name } = file;
-    const [showContextMenu, setShowContextMenu] = useState(false);
-    const [contextAction, setContextAction] = useState('');
-    return (    
-        <div className="explorer-file-item">
-            {
-                contextAction === 'edit' && (<ItemEditor value={name} onSubmitNewName={(newName) => {
-                    console.log(newName);
-                    setContextAction('');
-                }} />)
-            }
-            <div 
-                className={`explorer-file-item__head ${contextAction === 'edit' ? 'hide' : ''}`}
-                onContextMenu={(event) => {
-                    event.preventDefault();
-                    setShowContextMenu(!showContextMenu);
-                }}
-                data-testid="file-item-head"
-            >
-                <span className="material-icons">description</span>
-                { name }
-            </div>
-            <ExplorerContextMenu show={showContextMenu} handleMenuItemClick={(type) => {
-                setShowContextMenu(false)
-                setContextAction(type)
-            }} />
-        </div>
-    );
+  const { file } = props;
+  const { name } = file;
+  const [showContextMenu, setShowContextMenu] = useState(false);
+  const [contextAction, setContextAction] = useState('');
+  return (    
+    <div className="explorer-file-item">
+      {
+        contextAction === 'edit' && (<ItemEditor value={name} onSubmitNewName={(newName) => {
+          console.log(newName);
+          setContextAction('');
+          dispatch(updateFileName(file, newName));
+        }} />)
+      }
+      <div 
+        className={`explorer-file-item__head ${contextAction === 'edit' ? 'hide' : ''}`}
+        onContextMenu={(event) => {
+          event.preventDefault();
+          setShowContextMenu(!showContextMenu);
+        }}
+        data-testid="file-item-head"
+      >
+        <span className="material-icons">description</span>
+        { name }
+      </div>
+      <ExplorerContextMenu show={showContextMenu} handleMenuItemClick={(type) => {
+        setShowContextMenu(false)
+        setContextAction(type)
+      }} />
+    </div>
+  );
 };
 
 export default ExplorerFileItem;
